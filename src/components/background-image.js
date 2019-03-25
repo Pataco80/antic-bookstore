c5d3b4a48f5c2c53945347f0ce040fa2238c072b
Ricardo Do Vale <ricardodovale@imac-pro-de-ricardo-1.home>
2019-03-25 18:56:14 +0100

    initiation de backgroundimage

---
+++ src/components/background-image.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import BcgImg from 'gatsby-image'
+import { Container, Navbar, Nav } from 'react-bootstrap'
+
+
+export default function BackgroundImage() {
+ return (
+  <section>
+
+  </section>
+ )
+}
+
